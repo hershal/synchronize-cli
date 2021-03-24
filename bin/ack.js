8e32972b091967d1ca4d914efac611dc1bc9ef20
Hershal Bhave <hershal.bhave@gmail.com>
2021-03-23 23:49:50 -0500

    update packaging

--- bin/ack.js
+++ bin/ack.js
@@ -1,4 +1,4 @@
-'use strict';
+#!/usr/bin/env node
 
 const ipc = require('node-ipc');
 const constants = require('../lib/constants.js');
--- bin/syn.js
+++ bin/syn.js
@@ -1,4 +1,4 @@
-'use strict';
+#!/usr/bin/env node
 
 const ipc = require('node-ipc');
 const constants = require('../lib/constants.js');
@@ -6,6 +6,7 @@ const constants = require('../lib/constants.js');
 
 ipc.config.id = 'syn';
 ipc.config.retry = 1500;
+ipc.config.logger = () => {};
 
 ipc.connectTo(constants.appid, () => {
     ipc.of[constants.appid].on('connect', (data) => {
