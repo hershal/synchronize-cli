9cc6b5ae96e571ab70662f5da796bee505f13908
Hershal Bhave <hershal.bhave@gmail.com>
2021-03-24 21:45:08 -0500

    minor stylistic changes, added ava for testing

--- bin/ack.js
+++ bin/ack.js
@@ -5,7 +5,7 @@ const constants = require('../lib/constants.js');
 const debug = require('debug')('ack');
 
 const args = process.argv.slice(2);
-const topic = args.length == 0 ? 'default' : args[0];
+const topic = args.length === 0 ? 'default' : args[0];
 
 ipc.config.id = process.pid;
 ipc.config.retry = 1500;
@@ -15,76 +15,76 @@ startClientAsync();
 
 
 function handleSyn(data, socket, cb) {
-    if (data == topic) {
-        cb(data, socket);
-    }
+  if (data === topic) {
+    cb(data, socket);
+  }
 }
 
 function advertiseListen() {
-    debug('listening for ' + topic);
+  debug('listening for ' + topic);
 }
 
 
 function handleSynClient(data, socket) {
-    debug('client got syn: ' + data);
-    stopClientAsync();
+  debug('client got syn: ' + data);
+  stopClientAsync();
 }
 
 function startClientAsync() {
-    ipc.connectTo(constants.appid, () => {
-        ipc.of[constants.appid].on('error', (err) => {
-            ipc.disconnect(constants.appid);
-            startServerAsync();
-        });
-
-        ipc.of[constants.appid].on('connect', () => {
-            debug('connected');
-            advertiseListen()
-        });
-
-        ipc.of[constants.appid].on(constants.opcodes.syn, (data, socket) => {
-            handleSyn(data, socket, handleSynClient);
-        });
+  ipc.connectTo(constants.appid, () => {
+    ipc.of[constants.appid].on('error', (err) => {
+      ipc.disconnect(constants.appid);
+      startServerAsync();
     });
+
+    ipc.of[constants.appid].on('connect', () => {
+      debug('connected');
+      advertiseListen()
+    });
+
+    ipc.of[constants.appid].on(constants.opcodes.syn, (data, socket) => {
+      handleSyn(data, socket, handleSynClient);
+    });
+  });
 }
 
 function stopClientAsync() {
-    debug('stopping client...');
-    setTimeout(() => {
-        ipc.disconnect(constants.appid);
-        debug('stopping client... done.');
-    }, 0);
+  debug('stopping client...');
+  setTimeout(() => {
+    ipc.disconnect(constants.appid);
+    debug('stopping client... done.');
+  }, 0);
 }
 
 
 function startServerAsync() {
-    ipc.config.id = constants.appid;
-    let connectedSockets = [];
+  ipc.config.id = constants.appid;
+  let connectedSockets = [];
 
-    ipc.serve(() => {
-        debug('starting server... done.');
-        advertiseListen();
+  ipc.serve(() => {
+    debug('starting server... done.');
+    advertiseListen();
 
-        ipc.server.on(constants.opcodes.syn, (data, socket) => {
-            handleSyn(data, socket, handleSynServer);
-        });
+    ipc.server.on(constants.opcodes.syn, (data, socket) => {
+      handleSyn(data, socket, handleSynServer);
     });
+  });
 
-    debug('starting server...');
-    ipc.server.start();
+  debug('starting server...');
+  ipc.server.start();
 }
 
 function handleSynServer(data, socket) {
-    debug('server got syn: ' + data);
-    debug('broadcasting syn: ' + data);
-    ipc.server.broadcast(constants.opcodes.syn, data);
-    stopServerAsync();
+  debug('server got syn: ' + data);
+  debug('broadcasting syn: ' + data);
+  ipc.server.broadcast(constants.opcodes.syn, data);
+  stopServerAsync();
 }
 
 function stopServerAsync() {
-    debug('stopping server...');
-    setTimeout(() => {
-        ipc.server.stop();
-        debug('stopping server... done.');
-    }, 0);
+  debug('stopping server...');
+  setTimeout(() => {
+    ipc.server.stop();
+    debug('stopping server... done.');
+  }, 0);
 }
--- bin/syn.js
+++ bin/syn.js
@@ -5,7 +5,7 @@ const constants = require('../lib/constants.js');
 const debug = require('debug')('syn');
 
 const args = process.argv.slice(2);
-const topic = args.length == 0 ? 'default' : args[0];
+const topic = args.length === 0 ? 'default' : args[0];
 
 ipc.config.id = 'syn';
 ipc.config.retry = 1500;
@@ -13,22 +13,22 @@ ipc.config.logger = () => {};
 ipc.config.stopRetrying = true;
 
 ipc.connectTo(constants.appid, () => {
-    debug('connecting...');
+  debug('connecting...');
 
-    ipc.of[constants.appid].on('connect', () => {
-        debug('connecting... done.');
-        const data = topic;
-        debug('sent syn: ' + data);
-        ipc.of[constants.appid].emit(constants.opcodes.syn, data);
-        debug('disconnecting...');
-        setTimeout(() => {
-            ipc.disconnect(constants.appid);
-            debug('disconnecting... done.');
-        }, 0);
-    });
+  ipc.of[constants.appid].on('connect', () => {
+    debug('connecting... done.');
+    const data = topic;
+    debug('sent syn: ' + data);
+    ipc.of[constants.appid].emit(constants.opcodes.syn, data);
+    debug('disconnecting...');
+    setTimeout(() => {
+      ipc.disconnect(constants.appid);
+      debug('disconnecting... done.');
+    }, 0);
+  });
 
-    ipc.of[constants.appid].on('error', () => {
-        debug('no server found.');
-    });
+  ipc.of[constants.appid].on('error', () => {
+    debug('no server found.');
+  });
 });
 
