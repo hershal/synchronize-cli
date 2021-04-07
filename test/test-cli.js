68b49e7fc1c7ec7e3ade4f6e56632ba30ca01849
Hershal Bhave <hershal.bhave@gmail.com>
2021-04-07 18:35:22 -0500

    Add some tests before breaking shit

---
+++ test/test-cli.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const test = require('ava');
+const child_process = require('child_process');
+const process = require('process');
+
+process.env['DEBUG'] = '*';
+
+
+function wait(ms) {
+  if (ms === undefined) { ms = 100; }
+  return new Promise(resolve => {
+    setTimeout(resolve, ms);
+  });
+}
+
+
+function run(command) {
+  return new Promise(async (res, rej) => {
+    child_process.exec(`${__dirname}/../bin/${command}`, (err, stdout, stderr) => {
+      if (err) { rej(err); } else { res(); }
+    });
+  });
+}
+
+
+
+
+test.serial('basic ack/syn', async t => {
+  const r = run("ack.js");
+  await wait();
+
+  await run("syn.js");
+  await r.then(t.pass);
+});
+
+
+test.serial('multiple ack', async t => {
+  let promises = [];
+
+  promises.push(run("ack.js"));
+  await wait();
+  promises.push(run("ack.js"));
+  await wait();
+  promises.push(run("ack.js"));
+  await wait();
+
+  const r = Promise.all(promises);
+
+  await run("syn.js");
+  await r.then(t.pass);
+});
