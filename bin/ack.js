6bfbfbc35343b3b04a0532f6911c5acd46bf4e0d
Hershal Bhave <hershal.bhave@gmail.com>
2022-07-11 14:22:26 -0500

    move ack to its own file and add tests for race condition

--- bin/ack.js
+++ bin/ack.js
@@ -1,98 +1,21 @@
 #!/usr/bin/env node
 
-const constants = require('../lib/constants.js');
+const AckServer = require('../lib/ack.js').AckServer;
+
 
 const argv = require('yargs/yargs')(process.argv.slice(2))
       .default('count', 1)
       .argv;
 
 
-let classObj = {
-  debug: require('debug')('ack'),
-  ipc: require('node-ipc'),
-  channel: "",
-  count: 1
-};
+
+function main() {
+    const channel = argv._[0];
+    const count = argv.count;
+
+    const server = new AckServer(process.pid, channel, count);
+    server.start();
+}
 
 
 main();
-
-
-function main() {
-  classObj.ipc.config.id = process.pid;
-  classObj.ipc.config.retry = 1500;
-  classObj.ipc.config.logger = () => {};
-
-  classObj.channel = argv._.length === 0 ? constants.appid : argv._[0];
-  classObj.count = argv.count;
-  startClientAsync(classObj);
-}
-
-
-
-function handleSynClient(obj, data, socket) {
-  obj.debug('client got: ' + data);
-  stopClientAsync(obj);
-}
-
-function handleSynServer(obj, data, socket) {
-  obj.debug('server got: ' + data);
-  obj.debug('broadcasting: ' + data);
-  obj.ipc.server.broadcast(constants.opcodes.syn, data);
-
-  stopServerAsync(obj);
-}
-
-
-
-function startClientAsync(obj) {
-  obj.debug('channel: ' + obj.channel);
-  obj.debug('count: ' + obj.count);
-
-  obj.debug('connecting...');
-
-  obj.ipc.connectTo(obj.channel, () => {
-    obj.ipc.of[obj.channel].on('error', (err) => {
-      obj.debug('connecting... no server found');
-      obj.ipc.disconnect(obj.channel);
-      startServerAsync(obj);
-    });
-
-    obj.ipc.of[obj.channel].on('connect', () => {
-      obj.debug('connecting... done.');
-    });
-
-    obj.ipc.of[obj.channel].on(constants.opcodes.syn, (data, socket) => {
-      handleSynClient(obj, data, socket);
-    });
-  });
-}
-
-function stopClientAsync(obj) {
-  obj.debug('stopping client...');
-  obj.ipc.disconnect(obj.channel);
-  obj.debug('stopping client... done.');
-}
-
-
-function startServerAsync(obj) {
-  obj.ipc.config.id = obj.channel;
-  let connectedSockets = [];
-  obj.debug('starting server...');
-
-  obj.ipc.serve(() => {
-    obj.debug('starting server... done.');
-
-    obj.ipc.server.on(constants.opcodes.syn, (data, socket) => {
-      handleSynServer(obj, data, socket);
-    });
-  });
-
-  obj.ipc.server.start();
-}
-
-function stopServerAsync(obj) {
-  obj.debug('stopping server...');
-  obj.ipc.server.stop();
-  obj.debug('stopping server... done.');
-}
--- test/test-cli.js
+++ test/test-cli.js
@@ -3,6 +3,7 @@
 const test = require('ava');
 const child_process = require('child_process');
 const process = require('process');
+const uuid = require('../lib/uuid');
 
 process.env['DEBUG'] = '*';
 
@@ -15,12 +16,20 @@ function settle(ms) {
 }
 
 
+function settleLong(ms) {
+    if (ms === undefined) { ms = 1500; }
+    return new Promise(resolve => {
+        setTimeout(resolve, ms);
+    });
+}
+
+
 var processes = [];
 
 function run(command) {
   return new Promise((res, rej) => {
     const child = child_process.exec(`${__dirname}/../bin/${command}`, (err, stdout, stderr) => {
-      if (err) { rej(err); } else { res(); }
+        if (err) { rej(err); } else { res(); }
     });
     processes.push(child);
   });
@@ -28,7 +37,7 @@ function run(command) {
 
 /* Kill the tasks when we're done. We should get an unhandled exception error if
    something is still running and we never caught the exception. */
-test.afterEach.always(t => { processes.forEach((p) => p.kill("SIGINT")); processes = []; });
+test.afterEach.always(t => { processes.forEach((p) => p.kill("SIGTERM")); processes = []; });
 
 
 
@@ -108,3 +117,55 @@ test.serial('complex multi ack/syn with keyword', async t => {
 
   await p;
 });
+
+
+test.serial('trigger race condition', async t => {
+    const id = uuid();
+    const r0 = run(`DEBUG=* ack.js ${id}`).catch((err) => { /* do nothing */ });
+
+    /* grab the server */
+    if (processes.length != 1) {
+        t.fail();
+    }
+    const server = processes[0];
+
+    const r1 = run(`ack.js ${id}`).then(t.fail);
+    const r2 = run(`ack.js ${id}`).then(t.fail);
+    /* const r3 = run(`ack.js ${id}`).then(t.fail); */
+    /* const r4 = run(`ack.js ${id}`).then(t.fail); */
+    /* const r5 = run(`ack.js ${id}`).then(t.fail); */
+    /* const r6 = run(`ack.js ${id}`).then(t.fail); */
+    /* const r7 = run(`ack.js ${id}`).then(t.fail); */
+    /* const r8 = run(`ack.js ${id}`).then(t.fail); */
+    /* const r9 = run(`ack.js ${id}`).then(t.fail); */
+    await settle();
+
+    /* server exists, one of the others becomes the server */
+    server.kill();
+
+    /* nobody should exit */
+    await settleLong();
+    t.pass();
+})
+
+
+
+/* test.serial('basic ack with count', async t => { */
+/*   const r0 = run("ack.js --count 1"); */
+/*   await settle(); */
+/*   const r1 = run("ack.js --count 2"); */
+/*   await settle(); */
+/*   const r2 = run("ack.js --count 3"); */
+/*   await settle(); */
+
+/*   await run("syn.js"); */
+/*   await settle(); */
+/*   console.log([r0, r1, r2]); */
+
+/*   await run("syn.js"); */
+/*   await settle(); */
+/*   console.log([r0, r1, r2]); */
+
+/*   r2.then(t.fail).catch(() => {}); */
+/*   await Promise.all([r0, r1]).then(t.pass); */
+/* }); */
