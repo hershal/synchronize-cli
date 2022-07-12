497af86ce51fe78eafed34ec66b204ff603abe58
Hershal Bhave <hershal.bhave@gmail.com>
2022-07-12 11:57:38 -0500

    refactor syn; cleanups

--- bin/syn.js
+++ bin/syn.js
@@ -1,89 +1,26 @@
 #!/usr/bin/env node
 
-const ipc = require('node-ipc');
-const constants = require('../lib/constants.js');
-const debug = require('debug')('syn');
+const SynClient = require('../lib/syn.js').SynClient;
+
 const argv = require('yargs/yargs')(process.argv.slice(2))
       .default('count', 1)
+      .boolean('debug')
       .argv;
 
-const channel = argv._.length === 0 ? constants.appid : argv._[0];
-const count = argv.count;
 
-ipc.config.id = 'syn';
-ipc.config.retry = 1500;
-ipc.config.logger = () => {};
-ipc.config.stopRetrying = count;
+async function main() {
+    const channel = argv._[0];
+    const count = argv.count;
+    const debug = argv.debug;
 
-let pause = false;
-let kill = false;
-connect();
+    const client = new SynClient(process.pid, channel, count, debug);
 
-function connect() {
-    ipc.connectTo(channel, () => {
-        debug('channel: ' + channel);
-        debug('connecting...');
+    client.start();
 
-        ipc.of[channel].on('connect', () => {
-            if (kill == true) { return; }
-
-            debug('connecting... done.');
-            if (pause == true) {
-                debug('unpausing')
-                pause = false;
-            } else {
-                debug('initiating sendSyn');
-                setTimeout(() => sendSyn(count), 0);
-            }
-        });
-
-        ipc.of[channel].on('disconnect', () => {
-            debug('got disconnect');
-        })
-
-        ipc.of[channel].on(constants.opcodes.stop, () => {
-            if (kill == true) { return; }
-
-            debug('got stop, disconnecting and reconnecting');
-            debug('pausing')
-
-            pause = true;
-
-            ipc.disconnect(channel);
-            setTimeout(() => connect(), 100);
-        })
-
-        ipc.of[channel].on('error', () => {
-            if (kill == true) { return; }
-
-            kill = true;
-            debug('connecting... failed: no server found. Killing.');
-        });
+    process.on('SIGINT', () => {
+        client.stop();
     });
-
 }
 
 
-function sendSyn(count) {
-    if (kill == true) { return; }
-
-    if (count > 0) {
-        const newcount = count - 1;
-        const data = 1;
-
-        if (pause) {
-            debug(`sendSyn: waiting with count: ${count}`);
-            setTimeout(() => sendSyn(count), 1000);
-        } else {
-            debug(`sendSyn: sent: ${data}, count: ${count} -> ${newcount}`);
-            ipc.of[channel].emit(constants.opcodes.syn, data);
-            setTimeout(() => sendSyn(newcount), 1000);
-        }
-    } else {
-        debug('sendSyn: disconnecting...');
-        setTimeout(() => {
-            ipc.disconnect(channel);
-            debug('sendSyn: disconnecting... done.');
-        }, 0);
-    }
-}
+main();
