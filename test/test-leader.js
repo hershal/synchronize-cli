2a4d3cc2858c47598fcc7333a94dc657d0596d4c
Hershal Bhave <hershal.bhave@gmail.com>
2022-07-12 10:55:14 -0500

    add handoff tests

--- test/test-leader.js
+++ test/test-leader.js
@@ -146,3 +146,106 @@ test.serial('test multiple acks with server handoff', async t => {
 
     t.pass();
 });
+
+
+test.serial('test syn with count', async t => {
+    t.timeout(20000);
+    const uid = uuid();
+    const promises = Array(6).fill(true);
+    const count = (p) => p.reduce((a, c) => c == true ? a+1 : a, 0);
+
+    const r0 = run(`ack.js ${uid} --count=6`).then(() => promises[0] = false); await settle();
+    const r1 = run(`ack.js ${uid} --count=5`).then(() => promises[1] = false); await settle();
+    const r2 = run(`ack.js ${uid} --count=4`).then(() => promises[2] = false); await settle();
+    const r3 = run(`ack.js ${uid} --count=3`).then(() => promises[3] = false); await settle();
+    const r4 = run(`ack.js ${uid} --count=2`).then(() => promises[4] = false); await settle();
+    const r5 = run(`ack.js ${uid} --count=1`).then(() => promises[5] = false); await settle();
+
+    t.is(count(promises), 6);
+
+    await run(`syn.js ${uid} --count=2`); await settle();
+    t.is(count(promises), 4);
+
+    await run(`syn.js ${uid} --count=2`); await settle();
+    t.is(count(promises), 2);
+
+    await run(`syn.js ${uid} --count=2`); await settle();
+    t.is(count(promises), 0);
+
+    t.pass();
+});
+
+
+test.serial('test syn with count with server handoff', async t => {
+    t.timeout(20000);
+    const uid = uuid();
+    const promises = Array(6).fill(true);
+    const count = (p) => p.reduce((a, c) => c == true ? a+1 : a, 0);
+
+    const r0 = run(`ack.js ${uid} --count=1`).then(() => promises[0] = false); await settle();
+    const r1 = run(`ack.js ${uid} --count=2`).then(() => promises[1] = false); await settle();
+    const r2 = run(`ack.js ${uid} --count=3`).then(() => promises[2] = false); await settle();
+    const r3 = run(`ack.js ${uid} --count=4`).then(() => promises[3] = false); await settle();
+    const r4 = run(`ack.js ${uid} --count=5`).then(() => promises[4] = false); await settle();
+    const r5 = run(`ack.js ${uid} --count=6`).then(() => promises[5] = false); await settle();
+
+    t.is(count(promises), 6);
+
+    await run(`syn.js ${uid} --count=2`); await settle();
+    t.is(count(promises), 4);
+
+    await run(`syn.js ${uid} --count=2`); await settle();
+    t.is(count(promises), 2);
+
+    await run(`syn.js ${uid} --count=2`); await settle();
+    t.is(count(promises), 0);
+
+    t.pass();
+});
+
+
+test.serial('test syn with count with server handoff 2', async t => {
+    t.timeout(20000);
+    const uid = uuid();
+    const promises = Array(6).fill(true);
+    const count = (p) => p.reduce((a, c) => c == true ? a+1 : a, 0);
+
+    const r0 = run(`ack.js ${uid} --count=1`).then(() => promises[0] = false); await settle();
+    const r1 = run(`ack.js ${uid} --count=2`).then(() => promises[1] = false); await settle();
+    const r2 = run(`ack.js ${uid} --count=3`).then(() => promises[2] = false); await settle();
+    const r3 = run(`ack.js ${uid} --count=4`).then(() => promises[3] = false); await settle();
+    const r4 = run(`ack.js ${uid} --count=5`).then(() => promises[4] = false); await settle();
+    const r5 = run(`ack.js ${uid} --count=6`).then(() => promises[5] = false); await settle();
+
+    t.is(count(promises), 6);
+
+    await run(`syn.js ${uid} --count=5`); await settle();
+    t.is(count(promises), 1);
+
+    await run(`syn.js ${uid} --count=2`); await settle();
+    t.is(count(promises), 0);
+
+    t.pass();
+});
+
+
+test.serial('test syn with count with server handoff 3', async t => {
+    t.timeout(20000);
+    const uid = uuid();
+    const promises = Array(6).fill(true);
+    const count = (p) => p.reduce((a, c) => c == true ? a+1 : a, 0);
+
+    const r0 = run(`ack.js ${uid} --count=1`).then(() => promises[0] = false); await settle();
+    const r1 = run(`ack.js ${uid} --count=2`).then(() => promises[1] = false); await settle();
+    const r2 = run(`ack.js ${uid} --count=3`).then(() => promises[2] = false); await settle();
+    const r3 = run(`ack.js ${uid} --count=4`).then(() => promises[3] = false); await settle();
+    const r4 = run(`ack.js ${uid} --count=5`).then(() => promises[4] = false); await settle();
+    const r5 = run(`ack.js ${uid} --count=6`).then(() => promises[5] = false); await settle();
+
+    t.is(count(promises), 6);
+
+    await run(`syn.js ${uid} --count=10`); await settle();
+    t.is(count(promises), 0);
+
+    t.pass();
+});
